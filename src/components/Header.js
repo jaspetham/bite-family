83cf6143ba702a8c758169a677599bc8a609b898
jaspertham <jaspertham98@gmail.com>
2023-04-30 17:52:18 +0800

    first wave

---
+++ src/components/Header.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react'
+import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+
+function Header() {
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    useEffect(() => {
+        function handleScroll() {
+            setScrollPosition(window.scrollY);
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    },[])
+    const openToggle = (x) =>{
+        x.classList.toggle("change");
+        document.querySelector('.navWrap').classList.toggle('open');
+    }
+    return (
+        <nav className={scrollPosition > 0 ? 'nav items-center scrolled' : 'nav items-center'}>
+            <div className='imgBrand relative flex items-center'>
+                <a href="#">
+                    <img src={'/assets/images/logo_dark.jpg'} alt='logo'/>
+                </a>            
+            </div>
+            <div className='burgerWrap'>
+                <div className='burger' onClick={(event)=>{openToggle(event.currentTarget)}}>
+                    <div className='bar1'></div>
+                    <div className='bar2'></div>
+                    <div className='bar3'></div>
+                </div>
+            </div>
+            <div className='navWrap'>
+                <ul className='navLinks'>
+                    <li>
+                        <a href="" className='nav-link active'>Home</a>
+                    </li>
+                    <li>
+                        <a href="" className='nav-link'>About Us</a>
+                    </li>
+                    <li>
+                        <a href="" className='nav-link'>Diet Plans</a>
+                    </li>
+                    <li>
+                        <a href="" className='nav-link'>Pricing</a>
+                    </li>
+                    <li>
+                        <a href="" className='nav-link'>FAQ</a>
+                    </li>
+                    <li>
+                        <a href="" className='nav-link'>Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <ul className='socials'>
+                <li className="nav-link">
+                    <a href="" className='icon'>
+                        <FontAwesomeIcon icon={faFacebookF} />
+                    </a>
+                </li>
+                <li className="nav-link">
+                    <a href="" className='icon'>
+                        <FontAwesomeIcon icon={faInstagram} />
+                    </a>
+                </li>
+                <li className="nav-link">
+                    <a href="" className='icon'>
+                        <FontAwesomeIcon icon={faWhatsapp} />
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Header
---
+++ src/components/Hero.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Hero.css';
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+
+import banner1 from '../assets/banner1.jpg'
+import banner2 from '../assets/banner2.jpg'
+
+function Hero() {
+    let options = {
+        loop:false,
+        items:1,
+        nav:false,
+        dots:false
+    }
+    return (
+        <section id="Hero">
+            <OwlCarousel className='owl-theme' {...options}>
+                <div class='banner-wrap h-full'>
+                    <div className="banner-img bgImgCover h-full" style={{backgroundImage:banner1}}>
+                        <div className="banner-info">
+                            <h1 className="fs-700">Welcome to Bite Family</h1>
+                            <p className="fs-300">We keep you super healthy!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class='banner-wrap h-full'>
+                    <div className="banner-img bgImgCover h-full" style={{backgroundImage:banner2}}>
+                        <div className="banner-info">
+                            <h1 className="fs-700">Welcome to Bite Family</h1>
+                            <p className="fs-300">We keep you super healthy!</p>
+                        </div>
+                    </div>
+                </div>
+            </OwlCarousel>
+        </section>
+    )
+}
+
+export default Hero
