6d4e43df1aecefb7464970141b876bc6af798413
jaspertham <jaspertham98@gmail.com>
2023-06-04 21:45:55 +0800

    meet the team

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,6 +4,7 @@ import Title from './Title'
 import PortfolioImg from './PortfolioImg';
 import PortfolioFilter from './PortfolioFilter';
 import { motion, AnimatePresence } from 'framer-motion';
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 
 function Portfolio() {
     const [portfolios, setPortfolios] = useState([]);
@@ -13,52 +14,58 @@ function Portfolio() {
     useEffect(()=>{
         const imgs = [
             {
+                title:"Test",
+                subtitle:"lorem10",
+                tag:2,
+                src:"https://picsum.photos/400/450?image=1050",
+            },
+            {
                 title:"The Lazy Artist Gallery",
                 subtitle:"lorem10",
                 tag:1,
-                src:"https://github.com/OlgaKoplik/CodePen/blob/master/filterGallery/1.jpg?raw=true"
+                src:"https://picsum.photos/700/450?image=100"
             },
             {
                 title:"Daria Shevtsova",
                 subtitle:"lorem10",
                 tag:2,
-                src:"https://github.com/OlgaKoplik/CodePen/blob/master/filterGallery/2.jpg?raw=true"
+                src:"https://picsum.photos/200/300?image=230"
             },
             {
                 title:"Kasuma",
                 subtitle:"lorem10",
                 tag:3,
-                src:"https://github.com/OlgaKoplik/CodePen/blob/master/filterGallery/3.jpg?raw=true"
+                src:"https://picsum.photos/400/300?image=132"
             },
             {
                 title:"Dominika Roseclay",
                 subtitle:"lorem10",
                 tag:4,
-                src:"https://github.com/OlgaKoplik/CodePen/blob/master/filterGallery/4.jpg?raw=true"
+                src:"https://picsum.photos/400/300?image=140"
             },
             {
                 title:"Scott Webb",
                 subtitle:"lorem10",
                 tag:4,
-                src:"https://github.com/OlgaKoplik/CodePen/blob/master/filterGallery/5.jpg?raw=true"
+                src:"https://picsum.photos/400/400?image=420"
             },
             {
                 title:"Jeffrey Czum",
                 subtitle:"lorem10",
                 tag:1,
-                src:"https://github.com/OlgaKoplik/CodePen/blob/master/filterGallery/6.jpg?raw=true"
+                src:"https://picsum.photos/300/200?image=169"
             },
             {
                 title:"Dominika Roseclay",
                 subtitle:"lorem10",
                 tag:2,
-                src:"https://github.com/OlgaKoplik/CodePen/blob/master/filterGallery/7.jpg?raw=true"
+                src:"https://picsum.photos/400/300?image=423"
             },
             {
                 title:"Valeria Boltneva",
                 subtitle:"lorem10",
                 tag:3,
-                src:"https://github.com/OlgaKoplik/CodePen/blob/master/filterGallery/8.jpg?raw=true"
+                src:"https://picsum.photos/600/400?image=92"
             }
         ];
         setPortfolios(imgs);
@@ -67,23 +74,31 @@ function Portfolio() {
 
     return (
         <section id="Portfolio" className='bg-white clr-text section-gap'>
-            <div className="section-divider px-3">
-                <div className="flex flex-col gap-3">
-                    <Title title={'Portfolio'} primaryBg={true}/>
-                    <h4 className='fs-400'>Lorem ipsum dolor sit amet consectetur adipisicing praesentium nulla quod sequi reiciendis.</h4>
-                    <div className="portfolios-wrapper mt-4">
-                        <PortfolioFilter portfolios={portfolios} setFiltered={setFiltered} activeCategory={activeCategory} setActiveCategory={setActiveCategory}/>
-                        <motion.div 
-                            layout 
-                            className="portfolios mt-4"
-                        >
-                            <AnimatePresence>
-                                {filtered.map((img, index)=>{
-                                    return <PortfolioImg img={img} key={index}/>
-                                })}
-                            </AnimatePresence>
-                        </motion.div>
+            <div className="flex flex-col gap-3">
+                <div className="section-divider px-3">
+                    <div className="mb-4">
+                        <Title title={'Portfolio'} primaryBg={true}/>
+                        <h4 className='fs-400'>Lorem ipsum dolor sit amet consectetur adipisicing praesentium nulla quod sequi reiciendis.</h4>
                     </div>
+                    <PortfolioFilter portfolios={portfolios} setFiltered={setFiltered} activeCategory={activeCategory} setActiveCategory={setActiveCategory}/>
+                </div>
+                <div className="portfolios-wrapper mt-4">
+                    <motion.div 
+                        layout 
+                        className="portfolios mt-4"
+                    >
+                        <AnimatePresence>
+                            <ResponsiveMasonry
+                                columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
+                            >
+                                <Masonry columnsCount={4}>
+                                    {filtered.map((img, index)=>{
+                                        return <PortfolioImg img={img} key={index}/>
+                                    })}
+                                </Masonry>
+                            </ResponsiveMasonry>
+                        </AnimatePresence>
+                    </motion.div>
                 </div>
             </div>
         </section>
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,13 +1,49 @@
 import React from 'react'
 import './Team.css';
 import Title from './Title';
-
+const teams=[
+  {
+    name: 'Kington Lee',
+    position:'The Engineer',
+    shortIntro:'BITE Family’s R&D Department For Kitchen Hardwares',
+    image:"https://picsum.photos/500/500?image=1050"
+  },
+  {
+    name: 'Kent Wong',
+    position:'The Culinarian',
+    shortIntro:'Extensive Experience In Hotels & Restaurants',
+    image:"https://picsum.photos/500/500?image=206"
+  },
+  {
+    name: 'David Lee',
+    position:'The Culinarian',
+    shortIntro:'Extensive Experience In Hotels & Restaurants',
+    image:"https://picsum.photos/500/500?image=906"
+  },
+]
 function Team() {
   return (
     <section id="Team" className='bg-white clr-text section-gap'>
       <div className="section-divider px-3">
         <div className="flex flex-col gap-3">
           <Title title={'Meet Our Team'} primaryBg={true}/>
+            <div className="row">
+              {teams.map((team, i) => (
+                <div className="col-md-4 col-12">
+                  <div className="team">
+                    <img
+                      key={i}
+                      src={team.image}
+                    />
+                    <div className="team-detail p-3 text-white">
+                      <p className="fs-300 ff-bold">{team.name}</p>
+                      <p className="fs-100 ff-semi">{team.position}</p>
+                      <p className="fs-200">{team.shortIntro}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
         </div>
       </div>
     </section>
