272d4ef8a6c355972bc76602f8720f9f5baba72e
jaspertham <jaspertham98@gmail.com>
2023-06-26 00:28:20 +0800

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,9 +6,9 @@ function Map() {
     <section id="Map">
         <iframe 
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9475.290047360353!2d101.58310903659296!3d3.1293854113704307!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc4ff7a0d1893d%3A0x37e506a257b33902!2sBite%20Club%20Malaysia!5e0!3m2!1sen!2smy!4v1687709741729!5m2!1sen!2smy&zoom=0" 
-        allowfullscreen="" 
+        allowFullScreen="" 
         loading="lazy" 
-        referrerpolicy="no-referrer-when-downgrade"></iframe>
+        referrerPolicy="no-referrer-when-downgrade"></iframe>
     </section>
   )
 }
