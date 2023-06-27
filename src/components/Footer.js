c2906ec6a37e4e57b8126a12607becbe178a30ff
jaspertham <jaspertham98@gmail.com>
2023-06-27 22:31:54 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,29 +2,29 @@ import React from 'react'
 import './Footer.css'
 import { Image } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faMailBulk, faMapMarked, faMapMarkedAlt, faMapMarker, faMapMarkerAlt, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faMapMarkerAlt, faPhone } from '@fortawesome/free-solid-svg-icons'
 import { faFacebookF, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
   return (
-    <footer class="footer-distributed">
+    <footer className="footer-distributed">
 			<div className="row gap-2">
         <div className="col"></div>
         <div className="col-12 col-lg-4">
-          <div class="footer-left flex flex-col">
+          <div className="footer-left flex flex-col">
             <Image height={200} width={200} fluid src={'/assets/images/logo_white_fam.svg'}/>
-            <p class="footer-links flex gap-2 items-center my-3">
+            <p className="footer-links flex gap-2 items-center my-3">
               <a href="#Home">Home</a>
               <a href="#About">History</a>
               <a href="#Portfolio">Portfolio</a>
               <a href="#Pricing">Pricing</a>
             </p>
-            <p class="footer-company-name">Bite Family © 2023</p>
+            <p className="footer-company-name">Bite Family © 2023</p>
             </div>
         </div>
         <div className="col-12 col-lg-4">
-          <div class="footer-center flex flex-col gap-3">
-            <p class="footer-company-about">
+          <div className="footer-center flex flex-col gap-3">
+            <p className="footer-company-about">
               <span className='fs-200'>About the company</span>
               Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
             </p>
@@ -40,14 +40,14 @@ function Footer() {
               <FontAwesomeIcon className='icon fs-200' icon={faEnvelope}/>
               <p><a href="mailto:support@company.com">support@company.com</a></p>
             </div>
-            <div class="footer-icons flex gap-3">
-              <a href="#">
+            <div className="footer-icons flex gap-3">
+              <a href="#Footer">
                 <FontAwesomeIcon className='icon fs-200' icon={faFacebookF}/>
               </a>
-              <a href="#">
+              <a href="#Footer">
                 <FontAwesomeIcon className='icon fs-200' icon={faInstagram}/>
               </a>
-              <a href="#">
+              <a href="#Footer">
                 <FontAwesomeIcon className='icon fs-200' icon={faWhatsapp}/>
               </a>
             </div>
