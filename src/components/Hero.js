fd77eca5096a29734984be0db3653e58509c270f
jaspertham <jaspertham98@gmail.com>
2023-06-28 22:07:10 +0800

    update hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,17 +9,28 @@ import banner2 from '../assets/banner2.jpg'
 
 import flag1 from '../assets/flags/malaysia.png';
 import flag2 from '../assets/flags/singapore.png';
+import chevronLeft from '../assets/icons/circle-chevron-left-solid.svg';
+import chevronRight from '../assets/icons/circle-chevron-right-solid.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { faArrowDown, faArrowLeft, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 
 
 function Hero() {
+    const icon = () =>{
+        return(
+            <FontAwesomeIcon icon={faArrowLeft}/>
+        )
+    }
     let options = {
         loop:false,
         items:1,
-        nav:false,
-        dots:false
+        nav:true,
+        dots:false,
+        navText: [
+            `<div class='nav-button owl-prev'><img src=${chevronLeft}/></div>`, 
+            `<div class='nav-button owl-next'><img src=${chevronRight}/></div>`
+        ],
     }
     return (
         <section id="Hero">
@@ -29,7 +40,7 @@ function Hero() {
                         <div className="banner-info">
                             <Image fluid src={'/assets/images/logo_white_fam.svg'}/>
                             <div className="mt-3">
-                                <p className="fs-500">We keep you super healthy!</p>
+                                <p className="fs-400">We keep you super healthy!</p>
                                 <div className="flex flex-col justify-center gap-3 mt-5">
                                     <div className="flex justify-center gap-5">
                                         <a href="#About" className="btn-classic btn-transparent rounded-lg btn-hero fs-200 text-capitalize rounder-2xl">Store</a>
@@ -53,7 +64,7 @@ function Hero() {
                         <div className="banner-info">
                             <Image fluid src={'/assets/images/logo_white_fam.svg'}/>
                             <div className="mt-3">
-                                <p className="fs-500">We keep you super healthy!</p>
+                                <p className="fs-400">We keep you super healthy!</p>
                                 <div className="flex flex-col justify-center gap-3 mt-5">
                                     <div className="flex justify-center gap-5">
                                         <a href="#About" className="btn-classic btn-transparent rounded-lg btn-hero fs-200 text-capitalize rounder-2xl">Store</a>
@@ -70,7 +81,7 @@ function Hero() {
                 </div>
             </OwlCarousel>
             <div className="arrow">
-                <FontAwesomeIcon className='fs-400' icon={faArrowDown}/>
+                <FontAwesomeIcon className='fs-400' icon={faChevronDown}/>
             </div>
         </section>
     )
